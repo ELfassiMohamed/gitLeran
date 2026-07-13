73bca430f4f730de51a5041b3fa288fd7a395269
ELfassiMohamed <elfassimohamed2001@gmail.com>
2026-07-13 23:07:35 +0100

    models.py attempt 1

--- models.py
+++ models.py
@@ -9,11 +9,4 @@ class features:
 
     
 
-    def predict_all(self, features: dict) -> dict:
-        predictions = {}
-        for task, model in self.models.items():
-            pred = model.predict([list(features.values())])[0]
-            proba = max(model.predict_proba([list(features.values())])[0])
-            label = self.le_target[task].inverse_transform([pred])[0]
-            predictions[task] = {"label": label, "confidence": round(proba * 100, 1)}
-        return predictions
+    
