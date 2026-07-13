5a4895c60b02e0c6dbee2edbb07b112b27031a98
ELfassiMohamed <elfassimohamed2001@gmail.com>
2026-07-13 23:08:18 +0100

    models.py attempt 2

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ class features:
         self.le_target = {}    # {task: LabelEncoder} pour la cible
         self.X_means = {}      # moyennes des features
         self.feature_names = {}
-        self._is_trained = False
 
     
 
